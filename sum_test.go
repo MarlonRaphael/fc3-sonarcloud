d971bedbd7493999a377a39fba5c6a4417f7c181
Marlon Raphael <marlon.raphaael@hotmail.com>
2023-09-10 12:23:52 -0300

    ci: update .gitignore file and refactor sum_test

--- sum_test.go
+++ sum_test.go
@@ -1,11 +1,13 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-    result := sum(2, 3)
+	result := sum(2, 3)
 
-    if result != 5 {
-        t.Error("The result must be 5")
-    }
+	if result != 5 {
+		t.Error("The result must be 5")
+	}
 }
